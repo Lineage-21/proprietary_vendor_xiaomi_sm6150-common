e36f427547125673d8e61f85a3dd4da1d6e0b706
Leonardo Ledda <leonardoledda@gmail.com>
2023-10-06 20:47:13 +0200

    sm6150-common: Add prebuilt libprotobuf-cpp-lite-3.9.1 from redfin
    * Sensors and RIL need it
    
    Change-Id: I4c1e919b824c29f878fb218307143a77820e672d
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- sm6150-common-vendor.mk
+++ sm6150-common-vendor.mk
@@ -262,6 +262,7 @@ PRODUCT_PACKAGES += \
     libpdmapper \
     libpdnotifier \
     libperipheral_client \
+    libprotobuf-cpp-lite-3.9.1 \
     libqcbor \
     libqcc_file_agent \
     libqcmaputils \
